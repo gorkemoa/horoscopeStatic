8d553d148d58b84bea70c8bbbe66ae588f427de4
Görkem Öztürk <gorkem@Gorkemoa.local>
2024-08-15 11:50:22 +0300

    Models klasörünü ekledim

---
+++ lib/models/horoscopes.dart
@@ -0,0 +1,62 @@
+ final List<Map<String, String>> horoscopes =  [
+    {
+      'name': 'Koç',
+      'icon': '♈',
+      'details': 'Cesur ve enerjik. Koç burcu insanları lider ruhludur.',
+    },
+    {
+      'name': 'Boğa',
+      'icon': '♉',
+      'details': 'Sabırlı ve güvenilir. Boğa burcu insanları kararlıdır.',
+    },
+    {
+      'name': 'İkizler',
+      'icon': '♊',
+      'details': 'Meraklı ve uyumlu. İkizler burcu insanları sosyal ve iletişimcidir.',
+    },
+    {
+      'name': 'Yengeç',
+      'icon': '♋',
+      'details': 'Duygusal ve şefkatli. Yengeç burcu insanları ailelerine düşkündür.',
+    },
+    {
+      'name': 'Aslan',
+      'icon': '♌',
+      'details': 'Gururlu ve yaratıcı. Aslan burcu insanları kendine güvenlidir.',
+    },
+    {
+      'name': 'Başak',
+      'icon': '♍',
+      'details': 'Analitik ve dikkatli. Başak burcu insanları titiz ve çalışkandır.',
+    },
+    {
+      'name': 'Terazi',
+      'icon': '♎',
+      'details': 'Adil ve dengeli. Terazi burcu insanları uyumlu ve adaletlidir.',
+    },
+    {
+      'name': 'Akrep',
+      'icon': '♏',
+      'details': 'Tutkulu ve kararlı. Akrep burcu insanları gizemli ve güçlüdür.',
+    },
+    {
+      'name': 'Yay',
+      'icon': '♐',
+      'details': 'Macera sever ve iyimser. Yay burcu insanları özgürlüğüne düşkündür.',
+    },
+    {
+      'name': 'Oğlak',
+      'icon': '♑',
+      'details': 'Disiplinli ve sorumluluk sahibi. Oğlak burcu insanları hırslıdır.',
+    },
+    {
+      'name': 'Kova',
+      'icon': '♒',
+      'details': 'Bağımsız ve yenilikçi. Kova burcu insanları özgün ve zekidir.',
+    },
+    {
+      'name': 'Balık',
+      'icon': '♓',
+      'details': 'Hayalperest ve sezgisel. Balık burcu insanları duygusal ve yaratıcıdır.',
+    },
+  ];
---
+++ lib/models/users.dart
@@ -0,0 +1,4 @@
+const users={
+  'piksel@gmail.com':'piksel',
+  'gorkem@gmail.com':'123456',
+};
